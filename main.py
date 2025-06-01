5f66b60c693385530984f1799e078488dcd74728
alanandrewvarghese <alanandrew883@gmail.com>
2025-06-02 03:23:51 +0530

    Add CORS settings

--- main.py
+++ main.py
@@ -3,11 +3,20 @@ from features.ac_monitor.api import router as ac_router
 from features.solar_monitor.api import router as solar_router
 from features.summary.api import router as summary_router
 from common.logging import setup_logging
+from fastapi.middleware.cors import CORSMiddleware
+
 
 # Entry point for FastAPI app
 
 setup_logging()
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(ac_router)
 app.include_router(solar_router)
